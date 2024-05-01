5545f218ef19c8ec23a670f74a51cfb17fb8e094
guskirb <anguskirby1998@gmail.com>
2024-05-01 23:24:53 +0100

    Add render class with renderBoard method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+require('./style.css');
+
 const Board = require('./board');
 const Ship = require('./ship');
 const Player = require('./ship');
+
+class Render {
+    renderBoards() {
+        const playerBoard = document.querySelector('.playerBoard');
+        const enemyBoard = document.querySelector('.enemyBoard');
+
+        for (let x = 0; x < 10; x += 1) {
+            for (let y = 0; y < 10; y += 1) {
+                playerBoard.appendChild(document.createElement('div')).classList.add(`player[${x},${y}]`);
+                enemyBoard.appendChild(document.createElement('div')).classList.add(`enemy[${x},${y}]`);
+            }
+        }
+    }
+}
+
+const render = new Render;
+const board = new Board;
+board.createBoard();
+console.log(board.board)
+
+render.renderBoards();

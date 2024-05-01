7d89bdd6f24c25e5ead45dc011a547efcf51775c
guskirb <anguskirby1998@gmail.com>
2024-05-01 23:42:01 +0100

    Add initialize method

--- src/player.js
+++ src/player.js
@@ -1,12 +1,18 @@
+const Board = require('./board');
+const Ship = require('./ship');
+
 const NewPlayer = class Player {
     constructor(name) {
         this.name = name;
     }
 
-    assignBoard() {
-        const board = new Board;
-        board.createBoard()
-        this.board = board;
+    initialize() {
+        this.board = new Board;
+        this.board.createBoard();
+        this.battleship = new Ship(4);
+        this.destroyer = new Ship(3);
+        this.submarine = new Ship(3);
+        this.patrolboat = new Ship(2);
     }
 }
 

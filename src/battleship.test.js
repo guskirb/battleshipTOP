84d6615947f5bf15d63c2a38477b584cee694cd3
guskirb <anguskirby1998@gmail.com>
2024-05-01 06:07:33 +0100

    Add 'isSunk' method

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -2,40 +2,45 @@ class Ship {
     constructor(length) {
         this.length = length;
         this.hits = 0;
-        this.sunk = false;
     }
 
     hit() {
-        if (this.sunk){
+        if (this.isSunk()){
             return 'Error';
         }
         this.hits += 1;
+    }
+
+    isSunk() {
         if (this.hits === this.length) {
-            this.sunk = true;
+            return true;
         }
+        return false;
     }
 }
 
 const newShip = new Ship(2);
 
 it('returns ship object', () => {
-    expect(newShip).toEqual({ "hits": 0, "length": 2, "sunk": false });
+    expect(newShip).toEqual({ "hits": 0, "length": 2});
 })
 
 it('increases hit count', () => {
     expect(newShip.hit()).toBeUndefined;
-    expect(newShip).toEqual({ "hits": 1, "length": 2, "sunk": false });
+    expect(newShip).toEqual({ "hits": 1, "length": 2});
 })
 
 it('ship is sunk', () => {
     expect(newShip.hit()).toBeUndefined;
-    expect(newShip).toEqual({ "hits": 2, "length": 2, "sunk": true });
+    expect(newShip.isSunk()).toBeTruthy;
 })
 
 it('hit a sunken ship returns error', () => {
     expect(newShip.hit()).toBe('Error');
-    expect(newShip).toEqual({ "hits": 2, "length": 2, "sunk": true });
+    expect(newShip).toEqual({ "hits": 2, "length": 2});
 })
+
+
 
 
 
